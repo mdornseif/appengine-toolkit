8382ea02de6e3fae19945f958574082e065adf8f
Maximillian Dornseif <md@hudora.de>
2011-10-14 16:06:57 +0200

    Hotfix to work on current gae 1.5.5 / Python 2.7 runtime
    Also fixed an XSS exploit
    
    See also https://github.com/kamens/gae_mini_profiler/issues/13
    
    Change-Id: Ic32c2820232f36075e983b892aeec8ef6a792708
    Reviewed-on: http://gerrit.hudora.biz/1242
    Reviewed-by: Maximillian Dornseif <m.dornseif@hudora.de>
    Tested-by: Maximillian Dornseif <m.dornseif@hudora.de>

--- gaetk/gae_mini_profiler/middleware.py
+++ gaetk/gae_mini_profiler/middleware.py
@@ -67,10 +67,12 @@ class ProfilerWSGIMiddleware(object):
 
             def wrapped_appstats_app(environ, start_response):
                 # Use this wrapper to grab the app stats recorder for RequestStats.save()
-                if hasattr(recording.recorder, "get_for_current_request"):
-                    self.recorder = recording.recorder.get_for_current_request()
+                # on python27 we have a recorder_proxy on older runtimes a recorder
+                recorder = getattr(recording, 'recorder', getattr(recording, 'recorder_proxy'))
+                if hasattr(recorder, "get_for_current_request"):
+                    self.recorder = recorder.get_for_current_request()
                 else:
-                    self.recorder = recording.recorder
+                    self.recorder = recorder
                 return old_app(environ, start_response)
             self.app = recording.appstats_wsgi_middleware(wrapped_appstats_app)
 
@@ -81,7 +83,8 @@ class ProfilerWSGIMiddleware(object):
             # Get profiled wsgi result
             result = self.prof.runcall(lambda *args, **kwargs: self.app(environ, profiled_start_response), None, None)
 
-            self.recorder = recording.recorder
+            # on python27 we have a recorder_proxy on older runtimes a recorder
+            self.recorder = getattr(recording, 'recorder', getattr(recording, 'recorder_proxy'))
 
             # If we're dealing w/ a generator, profile all of the .next calls as well
             if type(result) == GeneratorType:
