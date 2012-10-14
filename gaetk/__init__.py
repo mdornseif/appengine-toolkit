f21c11cdb35a31364bd904c0ed05a39eb843b18b
Maximillian Dornseif <md@hudora.de>
2012-10-14 21:43:32 +0200

    import main submodules into base module

--- gaetk/__init__.py
+++ gaetk/__init__.py
@@ -4,10 +4,15 @@
 gaetk/__init__.py
 
 Created by Maximillian Dornseif on 2010-10-31.
-Copyright (c) 2010 HUDORA. All rights reserved.
+Copyright (c) 2010, 2012 HUDORA. All rights reserved.
 """
 
 try:
     import webapp2  # on AppEngine python27
 except ImportError:
     import mywebapp2 as webapp2  # on AppEngine python25
+
+import configuratione
+import handler
+import infrastructure
+import tools
