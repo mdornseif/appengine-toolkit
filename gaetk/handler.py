a192aa47ac1ecf44048e1324bea06aec6e7c0668
Maximillian Dornseif <md@hudora.de>
2012-11-04 03:51:00 +0100

    more roboust header geneartion

--- gaetk/handler.py
+++ gaetk/handler.py
@@ -444,7 +444,7 @@ class BasicHandler(webapp2.RequestHandler):
 
         if fmt not in ['html', 'json']:
             self.response.headers["Content-Disposition"] = \
-                                "%s; filename=%s.%s" % (disposition, filename, fmt)
+                                str("%s; filename=%s.%s" % (disposition, filename, fmt))
         # If we have gotten a `callback` parameter, we expect that this is a
         # [JSONP](http://en.wikipedia.org/wiki/JSONP#JSONP) can and therefore add the padding
         if self.request.get('callback', None) and fmt == 'json':
