ad76a4d59dd30d1990ab584fa290846a6d96c622
Maximillian Dornseif <md@hudora.de>
2015-04-09 15:21:04 +0200

    tools.slugify() added

--- gaetk/tools.py
+++ gaetk/tools.py
@@ -35,6 +35,22 @@ def get_cookie_domain():
         domain = '.'.join(host.split('.')[-2:])
     return domain
 
+## {{{ http://code.activestate.com/recipes/577257/ (r1)
+_slugify_strip_re = re.compile(r'[^\w\s-]')
+_slugify_hyphenate_re = re.compile(r'[-\s]+')
+def slugify(value):
+    """
+    Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+
+    From Django's "django/template/defaultfilters.py".
+    """
+    import unicodedata
+    value = unicodedata.normalize('NFKD', unicode(value)).encode('ascii', 'ignore')
+    value = unicode(_slugify_strip_re.sub('', value).strip().lower())
+    return _slugify_hyphenate_re.sub('-', value)
+## end of http://code.activestate.com/recipes/577257/ }}}
+
 
 if __name__ == "__main__":
     import doctest
