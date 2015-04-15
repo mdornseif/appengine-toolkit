3b6226135994ba2577c80a4a5a5174575d7e5639
Maximillian Dornseif <md@hudora.de>
2015-04-15 21:38:11 +0200

    modernes main;py

--- examples/main.py
+++ examples/main.py
@@ -6,16 +6,11 @@ main.py - central views for www.hudora.de
 Created by Maximillian Dornseif on 2015-02-26
 Copyright (c) 2015 HUDORA. All rights reserved.
 """
-
 import config
 
-import codecs
-import datetime
 import logging
-import re
 
 from google.appengine.api import memcache
-from google.appengine.ext import ndb
 import gae_mini_profiler.templatetags
 import gaetk
 import gaetk.defaulthandlers
@@ -51,25 +46,12 @@ class wwwHandler(gaetk.handler.BasicHandler):
             self.session['mc_cid'] = self.request.get('mc_cid')
             self.session['mc_eid'] = self.request.get('mc_eid')
 
-    def create_jinja2env(self, extensions=()):
-        """Erzeugt das jinja2 environment mit unseren Modifikationen."""
-        # global _jinja_env_cache
-        key = tuple(extensions)
-        if key not in _jinja_env_cache:
-            env = jinja2.Environment(loader=jinja2.FileSystemLoader(config.template_dirs),
-                                     extensions=extensions,
-                                     auto_reload=False,  # do check if the source changed
-                                     trim_blocks=True,  # first newline after a block is removed
-                                     bytecode_cache=jinja2.MemcachedBytecodeCache(memcache, timeout=600)
-                                     )
-            # Eigene Filter
-            env.globals['bottommenuurl'] = \
-                '/admin/'
-            env.globals['bottommenuaddon'] = '<i class="fa fa-area-chart"></i> Admin'
-            env.globals['huwawi_link'] = huwawi_link
-            env.globals['profiler_includes'] = gae_mini_profiler.templatetags.profiler_includes
-            _jinja_env_cache[key] = env
-        return _jinja_env_cache[key]
+    def add_jinja2env_globals(self, env):
+        # Eigene Filter
+        env.globals['bottommenuurl'] = '/admin/'
+        env.globals['bottommenuaddon'] = '<i class="fa fa-area-chart"></i> Admin'
+        env.globals['huwawi_link'] = huwawi_link
+        env.globals['profiler_includes'] = gae_mini_profiler.templatetags.profiler_includes
 
     def default_template_vars(self, values):
         """Default variablen für Breadcrumbs etc."""
@@ -84,48 +66,14 @@ class wwwHandler(gaetk.handler.BasicHandler):
         return values
 
 
-class MarkdownFileHandler(wwwHandler):
-    """Zeigt beliebige Markdown Files an."""
-    template_name = 'gaetk_markdown.html'
-
-    def get(self, path, *_args, **_kwargs):
-        path = path.strip('/')
-        path = re.sub(r'[^a-z/]', '', path)
-        if not path:
-            path = 'index'
-        path = path + '.markdown'
-        textfile = 'text/%s' % path
-
-        try:
-            with codecs.open(textfile, 'r', 'utf-8') as fileobj:
-                text = fileobj.read()
-                # wir gehen davon aus, dass die erste Zeile, die mit `# ` beginnt, der Titel ist
-                for line in text.split('\n'):
-                    if line.startswith('# '):
-                        self.title = line.lstrip('# ')
-                        break
-            self.render({'text': text, 'title': self.title}, self.template_name)
-        except IOError:
-            raise gaetk.handler.HTTP404_NotFound("%s not available" % textfile)
-
-
-class Warmup(wwwHandler):
-    """Instanz initialisieren"""
-
-    def authchecker(self, method, *args, **kwargs):
-        """Authentifizierung abschalten."""
-        pass
-
-    def get(self):
-        # _strptime importieren. hilft gegen
-        # http://groups.google.com/group/google-appengine-python/browse_thread/thread/efbcffa181c32f33
-        datetime.datetime.strptime('2000-01-01', '%Y-%m-%d').date()
-        self.return_text('ok')
+class MarkdownFileHandler(wwwHandler, gaetk.handler.MarkdownFileHandler):
+    """Markdown Files darstellen."""
+    pass
 
 
 def make_app(url_mapping, debug=config.DEBUG):
     """Generate a WSGI-App"""
-    app = ndb.toplevel(gaetk.webapp2.WSGIApplication(url_mapping, debug=debug))
+    app = gaetk.webapp2.WSGIApplication(url_mapping, debug=debug)
     # hier werden wir auch das Error-Handling verankern. aber noch nicht ...
     # app.error_handlers[500] = ErrorHandler
     return app
@@ -134,7 +82,7 @@ def make_app(url_mapping, debug=config.DEBUG):
 application = make_app([
     (r'^/version\.txt$', gaetk.defaulthandlers.VersionHandler),
     (r'^/robots\.txt$', gaetk.defaulthandlers.RobotTxtHandler),
-    (r'^/_ah/warmup$', Warmup),
+    (r'^/_ah/warmup$', gaetk.defaulthandlers.WarmupHandler),
     # (r'^/$', Homepage),
     (r'^(.*)$', MarkdownFileHandler),
-], config.DEBUG)
+], debug=config.DEBUG)
