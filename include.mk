c218a3a9caa63245a2d666ea7a4350d4ce0a2ca1
Maximillian Dornseif <md@hudora.de>
2016-12-16 09:32:50 +0100

    reduce number pf paralell pyflake8 to get rid of crashes

--- include.mk
+++ include.mk
@@ -53,7 +53,7 @@ PYLINT_ARGS= "--msg-template={path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" \
 LINT_FILES?= modules/ tests/*.py *.py lib/CentralServices/cs lib/appengine-toolkit/gaetk
 
 LINT_LINE_LENGTH= 110
-LINT_FLAKE8_ARGS= --max-complexity=12 --builtins=_ --exclude=appengine_config.py,lib/*.py --max-line-length=$(LINT_LINE_LENGTH) --ignore=E711,E712
+LINT_FLAKE8_ARGS= --jobs=1 --max-complexity=12 --builtins=_ --exclude=appengine_config.py,lib/*.py --max-line-length=$(LINT_LINE_LENGTH) --ignore=E711,E712
 MYPYTHONPATH := $(MYPYTHONPATH):lib/google_appengine:lib/google_appengine/lib/jinja2-2.6:./lib/google_appengine/lib/webob-1.2.3:./lib/google_appengine/lib/django-1.5:./lib/google_appengine/lib/webapp2-2.5.2
 
 default: check
