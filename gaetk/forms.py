b95f7b0ed4186b3bf67f21fc62713eab14824939
Christian Klein <c.klein@hudora.de>
2017-02-28 13:48:32 +0100

    Neue Datei forms.py

---
+++ gaetk/forms.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+"""
+forms.py - utility functions for form handling
+
+Created by Christian Klein on 2017-02-28.
+Copyright (c) 2017 HUDORA. All rights reserved.
+"""
+
+
+def get_changelist(form, obj):
+    """
+    Erstelle Liste mit Änderungen
+
+    Als Parameter wird ein validiertes Formular und das zugehörige Objekt erwartet.
+    Rückgabewert ist eine Liste von Dreitupeln bestehend aus dem Feldnamen,
+    dem Wert aus dem Formular und dem bestehenden Wert des Objekts.
+    """
+
+    changes = []
+    for field in form:
+        if hasattr(obj, field.name):
+            form_value, obj_value = field.data, getattr(obj, field.name)
+            if form_value != obj_value:
+                changes.append((field.name, form_value, obj_value))
+    return changes
