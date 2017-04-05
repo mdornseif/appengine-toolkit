845bb2fbf830b8704bce2295ede0d8e492347ede
Maximillian Dornseif <md@hudora.de>
2017-04-05 22:31:53 +0200

    avoid writing to non-existing properties

--- gaetk/infrastructure.py
+++ gaetk/infrastructure.py
@@ -133,8 +133,11 @@ def write_on_change(model, key, data, flush_cache=False):
 def write_on_change_instance(obj, data):
     """Schreibe Instanz mit geänderten Daten in Datastore."""
 
+    properties = compat.xdb_properties(obj)
     dirty = False
     for key, value in data.iteritems():
+        if key not in properties:
+            raise RuntimeError("unknown property %r" % key)
         if value != getattr(obj, key, None):
             setattr(obj, key, value)
             dirty = True
